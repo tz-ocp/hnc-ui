3869f1c8786ec9097e7147a826ab3eb8877a0c7e
Zahar <53610738+zfrhv@users.noreply.github.com>
2024-02-13 21:46:50 +0200

    Update server.js

--- image/server.js
+++ image/server.js
@@ -141,20 +141,18 @@ async function user_check_ns(req, res, ns_name) {
   
   await req.k8s_api.get(ns_template)
   .then(ns => {
-    console.log("got")
     console.log(ns_name)
     res.write(`data: ${JSON.stringify({
       type: "MODIFIED",
       object: ns
     })}\n\n`)
+    console.log(ns_name)
 
     // watch quotas + hrqs
     user_watch_quota(req, res, ns_name)
     user_watch_hrq(req, res, ns_name)
   })
   .catch(err => {
-    console.log("failed")
-    console.log(ns_name)
     res.write(`data: ${JSON.stringify({
       type: "DELETED",
       object: ns_template
